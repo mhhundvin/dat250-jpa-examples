ef6782221f20219f7f464bfadd48a8808b14d161
mhhundvin <mhhundvin@hotmail.com>
2021-09-08 15:18:27 +0200

    Working on tutorial step 6

--- eclipselink/jpa-basic/src/main/java/no/hvl/dat250/jpa/basicexample/Main.java
+++ eclipselink/jpa-basic/src/main/java/no/hvl/dat250/jpa/basicexample/Main.java
@@ -7,27 +7,28 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
 
+
 public class Main {
-    private static final String PERSISTENCE_UNIT_NAME = "todos";
+    private static final String PERSISTENCE_UNIT_NAME = "people";
     private static EntityManagerFactory factory;
 
     public static void main(String[] args) {
         factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
         EntityManager em = factory.createEntityManager();
         // read the existing entries and write to console
-        Query q = em.createQuery("select t from Todo t");
-        List<Todo> todoList = q.getResultList();
-        for (Todo todo : todoList) {
-            System.out.println(todo);
+        Query q = em.createQuery("select t from Person t");
+        List<Person> personList = q.getResultList();
+        for (Person person : personList) {
+            System.out.println(person);
         }
-        System.out.println("Size: " + todoList.size());
+        System.out.println("Size: " + personList.size());
 
-        // create new todo
+        // create new person
         em.getTransaction().begin();
-        Todo todo = new Todo();
-        todo.setSummary("This is a test");
-        todo.setDescription("This is a test");
-        em.persist(todo);
+        Person person = new Person();
+        person.setLastName("Nordmann");
+        person.setFirstName("Ola");
+        em.persist(person);
         em.getTransaction().commit();
 
         em.close();
